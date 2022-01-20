2b3d4a68f35f74cedbc3207ba15d76e217805eab
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-20 12:04:24 -0500

    removing test cases from wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -144,15 +144,3 @@ const foundWord = function(letterIndex,word) {
 };
 
 module.exports = wordSearch;
-
-// wordSearch([
-//   ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//   ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-//   ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//   ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-//   ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-//   ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-//   ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-//   ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-//   ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-// ], 'SEINFELD');
