0d52b2494186011fa62a7b07e3c75bcc2b80ecc0
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-20 11:44:36 -0500

    updating wordsearch.js and test_wordsearch.js

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,135 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal down and left", function() {  // check diagonal down and left
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'S'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'E', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'I', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal down and right", function() {  // check diagonal down and right
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'E'],
+      ['Y', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'F', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'L', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'D'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal up and right", function() {  // check diagonal up and right
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'D'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'E'],
+      ['Y', 'F', 'I', 'F', 'Q', 'E', 'E', 'L'],
+      ['H', 'M', 'J', 'N', 'F', 'F', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'N', 'E', 'R', 'L'],
+      ['B', 'F', 'I', 'I', 'N', 'E', 'Y', 'B'],
+      ['U', 'E', 'E', 'W', 'A', 'P', 'L', 'I'],
+      ['S', 'S', 'C', 'A', 'K', 'U', 'A', 'D'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal up and left", function() {  // check diagonal up and left
+    const result = wordSearch([
+      ['D', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'L', 'I', 'N', 'F', 'E', 'L', 'E'],
+      ['Y', 'F', 'E', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'F', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'N', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'I', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'E', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal down and left", function() {  // check diagonal down and left
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'S'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'A', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'B', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'C', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'D', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'F', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['G', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ABCDEFG');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal down and right", function() {  // check diagonal down and right
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'A', 'I', 'N', 'F', 'E', 'L', 'E'],
+      ['Y', 'F', 'B', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'C', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'D', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'F', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'G'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ABCDEFG');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal up and right", function() {  // check diagonal up and right
+    const result = wordSearch([
+      ['S', 'W', 'C', 'F', 'Q', 'U', 'A', 'D'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'G'],
+      ['Y', 'F', 'I', 'F', 'Q', 'E', 'F', 'L'],
+      ['H', 'M', 'J', 'N', 'F', 'E', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'D', 'E', 'R', 'L'],
+      ['B', 'F', 'I', 'C', 'N', 'E', 'Y', 'B'],
+      ['U', 'E', 'B', 'W', 'A', 'P', 'L', 'I'],
+      ['S', 'A', 'C', 'A', 'K', 'U', 'A', 'D'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ABCDEFG');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true for diagonal up and left", function() {  // check diagonal up and left
+    const result = wordSearch([
+      ['D', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['F', 'G', 'I', 'N', 'F', 'E', 'L', 'E'],
+      ['Y', 'F', 'F', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'E', 'E', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'D', 'D', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'C', 'C', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'B', 'B', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'A'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ABCDEFG');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,158 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const firstLetterPositions = findFirstLetterPositions(letters,word[0]);
 
-module.exports = wordSearch
+  for (const positions of firstLetterPositions) {
+    let startPos = [positions[0],positions[1]];
+    if (checkAllDirections(letters,word,startPos)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const findFirstLetterPositions = function(array,letter) {
+  const positions = [];
+
+  for (let y = 0; y < array.length; y++) {
+    for (let x = 0; x < array[y].length; x++) {
+      if (array[y][x] === letter) {
+        positions.push([y,x]);
+      }
+    }
+  }
+
+  return positions;
+};
+
+const checkAllDirections = function(array,word,start) {
+  if (checkX(array,word,start) || checkY(array,word,start) || checkXAndY(array,word,start)) {
+    return true;
+  }
+  return false;
+};
+
+const checkX = function(array,word,start) {
+  for (const directionX of [-1, 1]) {
+    let currentPos = [start[0], start[1] + directionX];
+    let letterIndex = 1;
+    let step = 1;
+    let currentXCondition = step < word.length && currentPos[1] >= 0 && currentPos[1] <= array[0].length - 1;
+    
+    while (currentXCondition) {
+      if (!nextLetterMatch(array,currentPos,word[letterIndex])) {
+        break;
+      }
+
+      letterIndex++;
+      if (foundWord(letterIndex,word)) {
+        console.log(`found the word on X axis starting at this position ${start[0]},${start[1]}`);
+        return true;
+      }
+
+      currentPos = [currentPos[0],currentPos[1] + directionX];
+      currentXCondition = step < word.length && currentPos[1] >= 0 && currentPos[1] <= array[0].length - 1;
+      step++;
+    }
+  }
+  return false;
+};
+
+const checkY = function(array,word,start) {
+  for (const directionY of [-1, 1]) {
+    let currentPos = [start[0] + directionY, start[1]];
+    let letterIndex = 1;
+    let step = 1;
+    let currentYCondition = step < word.length && currentPos[0] >= 0 && currentPos[0] <= array.length - 1;
+    
+    while (currentYCondition) {
+      if (!nextLetterMatch(array,currentPos,word[letterIndex])) {
+        break;
+      }
+
+      letterIndex++;
+      if (foundWord(letterIndex,word)) {
+        console.log(`found the word on Y axis starting at this position ${start[0]},${start[1]}`);
+        return true;
+      }
+
+      currentPos = [currentPos[0] + directionY,currentPos[1]];
+      currentYCondition = step < word.length && currentPos[0] >= 0 && currentPos[0] <= array.length - 1;
+      step++;
+    }
+  }
+  return false;
+};
+
+const checkXAndY = function(array,word,start) {
+  for (const directionY of [-1, 1]) {
+    for (const directionX of [-1, 1]) {
+      let currentPos = [start[0] + directionY, start[1] + directionX];
+      let letterIndex = 1;
+      let step = 1;
+      let currentXCondition = step < word.length && currentPos[1] >= 0 && currentPos[1] <= array[0].length - 1;
+      let currentYCondition = step < word.length && currentPos[0] >= 0 && currentPos[0] <= array.length - 1;
+    
+      while (currentXCondition && currentYCondition) {
+        if (!nextLetterMatch(array,currentPos,word[letterIndex])) {
+          break;
+        }
+
+        letterIndex++;
+        if (foundWord(letterIndex,word)) {
+          
+          if (directionY > 0) {
+            if (directionX > 0) {
+              console.log(`found the word on diagonal going down and right starting at this position ${start[0]},${start[1]}`);
+              return true;
+            } else {
+              console.log(`found the word on diagonal going down and left starting at this position ${start[0]},${start[1]}`);
+              return true;
+            }
+          } else {
+            if (directionX > 0) {
+              console.log(`found the word on diagonal going up and right starting at this position ${start[0]},${start[1]}`);
+              return true;
+            } else {
+              console.log(`found the word on diagonal going up and left starting at this position ${start[0]},${start[1]}`);
+              return true;
+            }
+          }
+        }
+
+        currentPos = [currentPos[0] + directionY,currentPos[1] + directionX];
+        currentXCondition = step < word.length && currentPos[1] >= 0 && currentPos[1] <= array[0].length - 1;
+        currentYCondition = step < word.length && currentPos[0] >= 0 && currentPos[0] <= array.length - 1;
+        step++;
+      }
+    }
+  }
+  return false;
+};
+
+const nextLetterMatch = function(array,currentPos,letter) {
+  if (array[currentPos[0]][currentPos[1]] !== letter) {
+    return false;
+  }
+  return true;
+};
+
+const foundWord = function(letterIndex,word) {
+  if (letterIndex === word.length) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
+
+// wordSearch([
+//   ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//   ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//   ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//   ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//   ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//   ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//   ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//   ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+// ], 'SEINFELD');
